b5f8001f2a5da7be12874185075952888d9b1111
dzendmitry <dzendmitry@gmail.com>
2018-05-02 18:56:03 +0300

    nn model without batching

--- svm2catboost.py
+++ svm2catboost.py
@@ -1,13 +1,13 @@
 
-inFile="TRAIN_data_out_model_full_cbow_v1.svm"
-outFile="TRAIN_data_out_model_full_cbow_v1.catboost"
+inFile="/Users/dzendmitry/dev/lab/liblinear-multicore-2.20/osx_amd64_openmp_bin/1000000_train.svm"
+outFile="1000000_train.catboost"
 
 with open(outFile, 'w') as wfile:
     with open(inFile) as rfile:
         i = 0
         for line in rfile:
             l = line.split(" ")
-            c = l[0]
+            c = int(l[0])-1
             attrs = [elem.split(":")[1].strip() for elem in l[1:]]
             attrs_line = "\t".join(attrs)
             wfile.write("{}\t{}\n".format(c, attrs_line))
